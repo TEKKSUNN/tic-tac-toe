293bd48ef60b7c17d718ffba4538e0f1a5fe32fd
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-08-21 18:54:38 +0800

    Add assign symbols function

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -9,5 +9,20 @@ const Game = (function() {
     const playGame = function() {
         const gameBoard = GameBoard();
     };
+
+    // Lets player decide what symbol or side they will play as
+    const assignSymbols = function() {
+        let playerSymbol = null;
+        const validSymbols = ["X", "O"];
+        while (!validSymbols.includes(playerSymbol)) {
+            playerSymbol = window.prompt("Play as? (X or O)").trim().toUpperCase();
+        }
+        let computerSymbol = "X";
+        if (playerSymbol === "X") {
+            computerSymbol = "O";
+        }
+        return { player: playerSymbol, computer: computerSymbol };
+    };
+
     return { playGame };
 })();
